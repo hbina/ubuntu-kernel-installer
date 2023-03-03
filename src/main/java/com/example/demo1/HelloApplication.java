a8d34ba8a78cca8f95cd51f95f491d579de4e769
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2023-03-03 11:25:33 +0800

    Now able to actually install
    The DEBs files are downloaded to /tmp and installation requires
    admin previlege.
    
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -2,9 +2,7 @@ package com.example.demo1;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
@@ -18,46 +16,47 @@ import javafx.stage.Stage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
 public class HelloApplication extends Application {
-
-    final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     final static String KERNEL_URL = "https://kernel.ubuntu.com/~kernel-ppa/mainline/";
     // Extend this regex to only match the right rows
     // Is it even worth it?
     final static Pattern PATTERN = Pattern.compile("v(\\d).*", Pattern.CASE_INSENSITIVE);
     /// Properties
     final static ObservableList<Kernel> availableKernelsObs = FXCollections.observableArrayList();
-    final static StringProperty distributionProperty = new SimpleStringProperty();
-    final static StringProperty architectureProperty = new SimpleStringProperty();
-    final static StringProperty currentKernelProperty = new SimpleStringProperty();
-    final static SimpleStringProperty selectedKernelVersionProperty = new SimpleStringProperty();
     final static ObservableList<String> selectedKernelDebsObs = FXCollections.observableArrayList();
+    final static StringProperty distributionProperty = new SimpleStringProperty("Unknown distribution");
+    final static StringProperty architectureProperty = new SimpleStringProperty("Unknown architecture");
+    final static StringProperty kernelVersionProperty = new SimpleStringProperty("Unknown kernel version");
+    final static StringProperty downloadButtonTextProperty = new SimpleStringProperty("Install");
+    final static BooleanProperty isInstallingProperty = new SimpleBooleanProperty(false);
+    static Kernel selectedKernel = null;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public static String runCommand(String... args) throws IOException {
+    public static String runCommand(List<String> args) throws IOException {
+        System.out.println("Running command: '" + String.join(" ", args) + "'");
         Process process = new ProcessBuilder(args).start();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         StringBuilder builder = new StringBuilder();
+
         String line;
         while ((line = reader.readLine()) != null) {
             builder.append(line);
+            builder.append(System.getProperty("line.separator"));
         }
+
         return builder.toString();
     }
 
@@ -68,8 +67,6 @@ public class HelloApplication extends Application {
         BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
         String inputLine = in.readLine();
 
-        // read every line of the HTML content in the URL
-        // and concat each line to the rawHTML string until every line is read.
         while (inputLine != null) {
             rawHTML.append(inputLine);
             inputLine = in.readLine();
@@ -79,12 +76,94 @@ public class HelloApplication extends Application {
         return rawHTML.toString();
     }
 
+    public static void triggerKernelDownload() {
+        if (isInstallingProperty.get() || selectedKernel == null) {
+            return;
+        }
+
+        System.out.println("Triggering download for " + selectedKernel.version);
+
+        isInstallingProperty.set(true);
+        downloadButtonTextProperty.set("Installing " + selectedKernel.version);
+
+        final var kernel = selectedKernel;
+        final var architecture = architectureProperty.get();
+        Runnable mainJob = () -> {
+            var downloadCounter = new AtomicInteger(kernel.debs.size());
+            var jobs = kernel.debs.stream().map((debName) -> new Thread(() -> {
+                try {
+                    var currentFile = new File(debName);
+
+                    if (currentFile.exists()) {
+                        if (!currentFile.delete()) {
+                            System.out.println("Unable to delete " + debName);
+                        }
+                    }
+
+                    // An example link:
+                    // https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.79/amd64/linux-headers-5.15.79-051579-generic_5.15.79-051579.202211160602_amd64.deb
+                    var urlStr = KERNEL_URL + kernel.version + "/" + architecture + "/" + debName;
+                    URL url = new URL(urlStr);
+                    ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
+                    try (FileOutputStream fileOutputStream = new FileOutputStream("/tmp/" + debName, false)) {
+                        fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+                        System.out.println("Finished downloading " + urlStr);
+                        downloadCounter.decrementAndGet();
+                    }
+                } catch (Exception e) {
+                    System.out.println("Unable to download " + kernel.version + " because " + e);
+                }
+            })).toList();
+
+            for (var job : jobs) {
+                job.start();
+            }
+
+            for (var job : jobs) {
+                try {
+                    job.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            var args = new ArrayList<String>(6 + kernel.debs.size());
+            Collections.addAll(args, "pkexec", "env", "DISPLAY=${DISPLAY}", "XAUTHORITY=${XAUTHORITY}", "dpkg", "--install");
+            for (var deb : kernel.debs) {
+                args.add("/tmp/" + deb);
+            }
+
+            if (downloadCounter.get() == 0) {
+                try {
+                    var output = runCommand(args);
+                    System.out.println(output);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Skipping install because one of the download failed");
+            }
+
+            Platform.runLater(() -> {
+                isInstallingProperty.set(false);
+                downloadButtonTextProperty.set("Install");
+            });
+        };
+        new Thread(mainJob).start();
+    }
+
 
     public static void curlIndex() {
         try {
-            distributionProperty.set(runCommand("lsb_release", "-sd"));
-            architectureProperty.set(runCommand("dpkg", "--print-architecture"));
-            currentKernelProperty.set(runCommand("uname", "-r"));
+            Platform.runLater(() -> {
+                try {
+                    distributionProperty.set(runCommand(Arrays.asList("lsb_release", "-sd")).trim());
+                    architectureProperty.set(runCommand(Arrays.asList("dpkg", "--print-architecture")).trim());
+                    kernelVersionProperty.set(runCommand(Arrays.asList("uname", "-r")).trim());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
 
             // create url with the string.
             String index = curlLink(KERNEL_URL);
@@ -115,7 +194,6 @@ public class HelloApplication extends Application {
             // The first 2 and the last rows are header related stuff.
             // Skip
             for (var kernel : kernels) {
-                // TODO: Figure out the currently installed version and append
                 var thread = new Thread(() -> {
                     try {
                         var debs = curlKernelDebs(kernel.version);
@@ -123,8 +201,8 @@ public class HelloApplication extends Application {
                             kernel.debs.addAll(debs);
                             Platform.runLater(() -> availableKernelsObs.add(kernel));
                         }
-                    } catch (Exception e) {
-                        // System.out.println("Skipping " + kernel.getVersion() + " because it does not contain any DEB files.");
+                    } catch (Exception ignored) {
+                        // Ignore versions that don't have DEB files
                     }
                 });
                 thread.setPriority(Thread.MIN_PRIORITY);
@@ -134,7 +212,6 @@ public class HelloApplication extends Application {
             System.out.println("Done!");
         } catch (Exception e) {
             e.printStackTrace();
-            // TODO: Show error to user
         }
     }
 
@@ -146,7 +223,7 @@ public class HelloApplication extends Application {
         architectureText.textProperty().bind(architectureProperty);
 
         Label currentKernelText = new Label();
-        currentKernelText.textProperty().bind(currentKernelProperty);
+        currentKernelText.textProperty().bind(kernelVersionProperty);
 
         HBox box = new HBox(distributionText, architectureText, currentKernelText);
         box.setSpacing(8);
@@ -174,8 +251,9 @@ public class HelloApplication extends Application {
         table.setRowFactory(tv -> {
             TableRow<Kernel> row = new TableRow<>();
             row.setOnMouseClicked(event -> {
-                selectedKernelDebsObs.clear();
-                selectedKernelDebsObs.addAll(row.getItem().debs);
+                var kernel = row.getItem();
+                selectedKernelDebsObs.setAll(kernel.debs);
+                selectedKernel = kernel;
             });
             return row;
         });
@@ -200,13 +278,15 @@ public class HelloApplication extends Application {
     }
 
     public static VBox generateBottomBox(TableView<Kernel> kernelsTable) {
-        ListView<String> lv = new ListView<>(selectedKernelDebsObs);
+        var lv = new ListView<>(selectedKernelDebsObs);
         lv.addEventFilter(MouseEvent.MOUSE_PRESSED, Event::consume);
 
-        Button button = new Button("Install");
-        button.disableProperty().bind(kernelsTable.getSelectionModel().selectedItemProperty().isNull());
+        var button = new Button();
+        button.textProperty().bind(downloadButtonTextProperty);
+        button.disableProperty().bind(kernelsTable.getSelectionModel().selectedItemProperty().isNull().or(isInstallingProperty));
+        button.setOnAction((e) -> triggerKernelDownload());
 
-        VBox vbox = new VBox(lv, button);
+        var vbox = new VBox(lv, button);
         vbox.setSpacing(8);
 
         return vbox;
